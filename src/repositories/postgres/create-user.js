64b12ab4733b2d6b03766388624fb0c4c68df75c
Marcelo Cardoso <marcelocardoshenrique@gmail.com>
2026-07-24 15:56:00 -0300

    fix: ensure created user is returned on create user repository

--- src/repositories/postgres/create-user.js
+++ src/repositories/postgres/create-user.js
@@ -1,17 +1,21 @@
-import { PostgresHelper } from "../../db/postgres/helper";
+import { PostgresHelper } from "../../db/postgres/helper.js";
 
 export class PostgresCreateUserRepository{
     async execute(createUserParams) {
-       const result = await PostgresHelper.query(
-            'INSERT INTO users (ID, first_name, last_name, email, password) VALUES ($1, $2, $3, $4, $5)',
+        await PostgresHelper.query(
+            'INSERT INTO users (id, first_name, last_name, email, password) VALUES ($1, $2, $3, $4, $5) RETURNING *',
             [
-                createUserParams.ID,
+                createUserParams.id,
                 createUserParams.first_name,
                 createUserParams.last_name,
                 createUserParams.email,
                 createUserParams.password
             ]
         );
-        return result[0];
+
+        const createdUser = await PostgresHelper.query('SELECT * FROM users WHERE id = $1',
+            [createUserParams.id]
+        )
+        return createdUser[0];
     }
 }
